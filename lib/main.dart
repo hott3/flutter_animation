efe9000564e8837c513355fc4f47bd4b8172285a
hott3 <113865871+hott3@users.noreply.github.com>
2023-12-10 23:31:44 +0900

    animationsパッケージで詳細画面の追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -37,24 +38,46 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Column(
+      body: Column(
         children: [
-          SizedBox(height: 16),
+          const SizedBox(height: 16),
           Row(
             children: <Widget>[
               Expanded(
-                child: _ContentsCard(
-                  imagePath: 'https://placehold.jp/300x300.png?text=dog',
-                  title: 'Title',
-                  subTitle: 'This is dog.',
+                child: OpenContainer(
+                  closedBuilder: ((context, _) {
+                    return const _ContentsCard(
+                      imagePath: 'https://placehold.jp/300x300.png?text=dog',
+                      title: 'Title',
+                      subTitle: 'This is dog.',
+                    );
+                  }),
+                  openBuilder: ((context, _) {
+                    return const _ContentsPage(
+                      imagePath: 'https://placehold.jp/300x300.png?text=dog',
+                      title: 'Title',
+                      subTitle: 'This is dog. Dogs are so cool.',
+                    );
+                  }),
                 ),
               ),
-              SizedBox(width: 8.0),
+              const SizedBox(width: 8.0),
               Expanded(
-                child: _ContentsCard(
-                  imagePath: 'https://placehold.jp/300x300.png?text=cat',
-                  title: 'Title',
-                  subTitle: 'This is cat.',
+                child: OpenContainer(
+                  closedBuilder: ((context, _) {
+                    return const _ContentsCard(
+                      imagePath: 'https://placehold.jp/300x300.png?text=cat',
+                      title: 'Title',
+                      subTitle: 'This is cat.',
+                    );
+                  }),
+                  openBuilder: ((context, _) {
+                    return const _ContentsPage(
+                      imagePath: 'https://placehold.jp/300x300.png?text=cat',
+                      title: 'Title',
+                      subTitle: 'This is cat. Cats are so cute.',
+                    );
+                  }),
                 ),
               ),
             ],
@@ -110,6 +133,53 @@ class _ContentsCard extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ContentsPage extends StatelessWidget {
+  const _ContentsPage({
+    required this.imagePath,
+    required this.title,
+    required this.subTitle,
+  });
+
+  final String imagePath;
+  final String title;
+  final String subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Read more'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            color: Colors.black38,
+            child: Center(
+              child: Image.network(
+                imagePath,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(title, style: Theme.of(context).textTheme.headlineMedium),
+                const SizedBox(height: 10),
+                Text(
+                  subTitle,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ],
             ),
           ),
         ],
